d8c5b09ec8b857eb3af60d55d423cb13d19f4442
Dmitry Shumilin <infernozzz@yandex.ru>
2021-05-06 19:30:03 +0300

    0.0.1: initial

---
+++ subsbu.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Subsbu
+ * Plugin URI: https://github.com/chirontex/subsbu
+ * Description: Плагин, позволяющий пользователям записываться на мероприятия, созданные в плагине myEventON.
+ * Version: 0.0.1
+ * Author: Dmitry Shumilin
+ * Author URI: mailto://chirontex@yandex.ru
+*/
+require_once __DIR__.'/vendor/autoload.php';
